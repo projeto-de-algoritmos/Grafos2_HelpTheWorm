f8839b335a3aedb6134b02fe200c21841dc6ebe9
Ailton Aires Amado <ailtonaires5@gmail.com>
2022-12-03 21:53:01 -0300

    feat: grid matrix creation

--- maze.js
+++ maze.js
@@ -39,7 +39,32 @@ class Maze {
 
   }
 
+  // Set the grid: Create new this.grid array based on number of instance rows and columns
+  setup() {
+    for (let r = 0; r < this.rows; r++) {
+      let row = [];
+      for (let c = 0; c < this.columns; c++) {
+        // Create a new instance of the Cell class for each element in the 2D array and push to the maze grid array
+        let cell = new Cell(r, c, this.grid, this.size);
+        row.push(cell);
+      }
+      this.grid.push(row);
+    }
+    // Set the starting grid
+    current = this.grid[0][0];
+    this.grid[this.rows - 1][this.columns - 1].goal = true;
 
+
+    this.res_egg[0] = randomNumber(0, this.rows - 1);
+    console.log("res_egg: ", this.res_egg[0]);
+    this.grid[this.res_egg[0][0]][this.res_egg[0][1]].egg = true;
+    this.res_egg[1] = randomNumber(0, this.rows - 1);
+    console.log("res_egg2: ", this.res_egg[1]);
+    this.grid[this.res_egg[1][0]][this.res_egg[1][1]].egg2 = true;
+    this.res_egg[2] = randomNumber(0, this.rows - 1);
+    console.log("res_egg3: ", this.res_egg[2]);
+    this.grid[this.res_egg[2][0]][this.res_egg[2][1]].egg3 = true;
+  }
 
  
 }
